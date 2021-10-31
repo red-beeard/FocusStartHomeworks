7ca67291671bcb4062310089d0740d4be84f7bc4
red-beeard <nifon1998@yandex.ru>
2021-10-31 18:06:00 +0300

    enum для команд

---
+++ CarApp/CarApp/Command.swift
@@ -0,0 +1,13 @@
+//
+//  Command.swift
+//  CarApp
+//
+//  Created by Red Beard on 31.10.2021.
+//
+
+enum Command: String {
+    case add = "Добавление нового автомобиля"
+    case show = "Вывод списка добавленных автомобилей"
+    case showFilter = "Вывод списка автомобилей с использованием фильтра по типу кузова"
+    case exit = "Выход"
+}
