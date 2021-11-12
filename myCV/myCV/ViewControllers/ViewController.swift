8106fe57a171f4642219146c23f4993587b7e421
red-beeard <nifon1998@yandex.ru>
2021-11-12 21:02:21 +0300

    Инициализация проекта без основного сториборда

---
+++ myCV/myCV/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myCV
+//
+//  Created by Red Beard on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
