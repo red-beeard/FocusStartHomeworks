299b9ec4542c1650ee3310a51b7547447db5b208
red-beeard <nifon1998@yandex.ru>
2021-11-16 21:38:47 +0300

    Создал CollectionApp

---
+++ CollectionApp/CollectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionApp
+//
+//  Created by Red Beard on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
