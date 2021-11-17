ff6636048f08068593e049a7140f220fe146a0c3
red-beeard <nifon1998@yandex.ru>
2021-11-17 17:24:22 +0300

    Сделал модель для хранения инфы о картинках

---
+++ CollectionApp/CollectionApp/Model/Image.swift
@@ -0,0 +1,19 @@
+//
+//  Image.swift
+//  CollectionApp
+//
+//  Created by Red Beard on 17.11.2021.
+//
+
+import Foundation
+
+struct Image {
+    let author: String
+    let filename: String
+    
+    static getImages() -> [Image] {
+        DataManager.shared.images.map { (key, value) in
+            Image(author: key, filename: value)
+        }
+    }
+}
