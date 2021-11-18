15992fc43f757234bfcd6dbd2104f3cc86c2dd96
red-beeard <nifon1998@yandex.ru>
2021-11-18 21:15:24 +0300

    Доделал экран с основным ViewConroller

--- CollectionApp/CollectionApp/ViewControllers/ViewController.swift
+++ CollectionApp/CollectionApp/ViewControllers/ViewController.swift
@@ -8,11 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private enum Metrics {
+        static let smallIndent = CGFloat(5)
+    }
+    
+    private let images = Image.getImages()
+    private var collectionView: UICollectionView! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Фотографии"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.view.backgroundColor = .systemBackground
+        
+        configuireCollectionView()
+        configurationLayout()
     }
+    
+    private func configuireCollectionView() {
+        let layout = FlowLayout()
+        self.collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
+        self.collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
+        self.collectionView.dataSource = self
+        self.collectionView.showsVerticalScrollIndicator = false
+    }
+    
+    private func configurationLayout() {
+        self.view.addSubview(self.collectionView)
+        
+        NSLayoutConstraint.activate([
+            self.collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
 
+// MARK: - UICollectionViewDataSource
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as! PhotoCell
+        
+        cell.image = self.images[indexPath.item]
+        
+        return cell
+    }
+    
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
 }
 
