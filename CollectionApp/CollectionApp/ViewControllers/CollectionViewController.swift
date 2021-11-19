ee22eb4af7bc6a2b7453a1d10f78e674b10c3b69
red-beeard <nifon1998@yandex.ru>
2021-11-19 23:47:28 +0300

    Добавил self, убрал nil у collectionView Добавил метод для перехода на следующее окно

--- CollectionApp/CollectionApp/ViewControllers/CollectionViewController.swift
+++ CollectionApp/CollectionApp/ViewControllers/CollectionViewController.swift
@@ -14,7 +14,7 @@ class CollectionViewController: UIViewController {
     }
     
     private let images = Image.getImages()
-    private var collectionView: UICollectionView! = nil
+    private var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +23,8 @@ class CollectionViewController: UIViewController {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.view.backgroundColor = .systemBackground
         
-        configuireCollectionView()
-        configurationLayout()
+        self.configuireCollectionView()
+        self.configurationLayout()
     }
     
     private func configuireCollectionView() {
@@ -33,6 +33,7 @@ class CollectionViewController: UIViewController {
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         self.collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
         self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         self.collectionView.showsVerticalScrollIndicator = false
     }
     
@@ -40,10 +41,10 @@ class CollectionViewController: UIViewController {
         self.view.addSubview(self.collectionView)
         
         NSLayoutConstraint.activate([
-            self.collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            self.collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            self.collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            self.collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            self.collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
     }
     
@@ -54,7 +55,7 @@ class CollectionViewController: UIViewController {
 extension CollectionViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
+        return self.images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -73,6 +74,12 @@ extension CollectionViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
         return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let photoVC = PhotoViewController()
+        photoVC.image = self.images[indexPath.item]
+        self.navigationController?.pushViewController(photoVC, animated: true)
     }
     
 }
