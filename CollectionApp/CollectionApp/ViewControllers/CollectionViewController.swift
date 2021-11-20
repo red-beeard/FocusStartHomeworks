140f795b392b0d06ed70bf35651e98b20e4993d0
red-beeard <nifon1998@yandex.ru>
2021-11-20 13:14:00 +0300

    Collection view на DiffableDataSource

--- CollectionApp/CollectionApp/ViewControllers/CollectionViewController.swift
+++ CollectionApp/CollectionApp/ViewControllers/CollectionViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class CollectionViewController: UIViewController {
     
+    private enum Constants {
+        static let sectionHeader = "section-header-element-kind"
+    }
+    
     private enum Metrics {
         static let smallIndent = CGFloat(5)
     }
     
     private let images = Image.getImages()
     private var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Int, Int>! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +30,19 @@ class CollectionViewController: UIViewController {
         
         self.configuireCollectionView()
         self.configurationLayout()
+        self.configureDataSource()
     }
     
     private func configuireCollectionView() {
-        let layout = FlowLayout()
+//        let layout = FlowLayout()
+        let layout = self.createCompositionalLayout()
+        
         self.collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         self.collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.identifier)
-        self.collectionView.dataSource = self
-        self.collectionView.delegate = self
         self.collectionView.showsVerticalScrollIndicator = false
+        
+        self.collectionView.delegate = self
     }
     
     private func configurationLayout() {
@@ -48,22 +56,54 @@ class CollectionViewController: UIViewController {
         ])
     }
     
-}
-
-// MARK: - UICollectionViewDataSource
-
-extension CollectionViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.images.count
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let leadingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)
+        ))
+        leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let trailingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)
+        ))
+        trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(0.33)),
+            subitems: [leadingItem, trailingItem, trailingItem])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.identifier, for: indexPath) as! PhotoCell
+    private func configureDataSource() {
         
-        cell.image = self.images[indexPath.item]
+        let cellRegistration = UICollectionView.CellRegistration<PhotoCell, Int> { cell, indexPath, itemIdentifier in
+            cell.image = self.images[itemIdentifier]
+        }
         
-        return cell
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: self.collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Int) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+//            cell.image = self.images[indexPath.item]
+            return cell
+            }
+        
+//        let supplementaryRegistration = UICollectionView.SupplementaryRegistration<TitleSupplementaryView>(elementKind: Constants.sectionHeader) { supplementaryView, elementKind, indexPath in
+//            supplementaryView.label.text = "\(indexPath.section)st section"
+//        }
+//
+//        dataSource.supplementaryViewProvider = { (view, kind, index) in
+//            return self.collectionView.dequeueConfiguredReusableSupplementary(
+//                using: supplementaryRegistration, for: index)
+//        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Array(0 ..< self.images.count))
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
     
 }
