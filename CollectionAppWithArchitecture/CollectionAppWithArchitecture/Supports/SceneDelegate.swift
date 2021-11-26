09f3e97d410b79f2b103a32ee0c8dd4637107864
red-beeard <nifon1998@yandex.ru>
2021-11-26 19:10:00 +0300

    Поправил AppDelegate и SceneDelegate

--- CollectionAppWithArchitecture/CollectionAppWithArchitecture/Supports/SceneDelegate.swift
+++ CollectionAppWithArchitecture/CollectionAppWithArchitecture/Supports/SceneDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: scene)
-        window.rootViewController = ViewController()
+        window.rootViewController = UINavigationController(rootViewController: CollectionViewController())
         window.makeKeyAndVisible()
         self.window = window
     }
