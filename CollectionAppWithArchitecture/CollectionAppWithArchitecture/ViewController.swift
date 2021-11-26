d0af2a2a3cb8f469cb867313340565e3527cfdbc
red-beeard <nifon1998@yandex.ru>
2021-11-26 17:44:08 +0300

    Создал проект

---
+++ CollectionAppWithArchitecture/CollectionAppWithArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionAppWithArchitecture
+//
+//  Created by Red Beard on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
