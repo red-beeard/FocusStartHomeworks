9299116ee4afbddc3a08bd4c376c5f5b5ccdc85c
red-beeard <nifon1998@yandex.ru>
2021-12-02 19:54:37 +0300

    initial commit

---
+++ CarAppMobile/CarAppMobile/Supports/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  CarAppMobile
+//
+//  Created by Red Beard on 02.12.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+    
+}
+
---
+++ CarAppMobile/CarAppMobile/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CarAppMobile
+//
+//  Created by Red Beard on 02.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
