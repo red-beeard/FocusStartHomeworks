bc00977f44eff4229c91089abba7aecb1aa7a4e1
red-beeard <nifon1998@yandex.ru>
2021-12-06 20:20:15 +0300

    Поправил импорты библиотек

--- CarAppMobile/CarAppMobile/SelectBrandScreen/SelectBrandAssembly.swift
+++ CarAppMobile/CarAppMobile/SelectBrandScreen/SelectBrandAssembly.swift
@@ -5,7 +5,6 @@
 //  Created by Red Beard on 03.12.2021.
 //
 
-import Foundation
 import UIKit
 
 final class SelectBrandAssembly {
--- CarAppMobile/CarAppMobile/SelectBrandScreen/SelectBrandPresenter.swift
+++ CarAppMobile/CarAppMobile/SelectBrandScreen/SelectBrandPresenter.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import UIKit
 
 protocol ISelectBrandPresenter {
     func loadView(controller: ISelectBrandViewController, view: ISelectBrandView)
