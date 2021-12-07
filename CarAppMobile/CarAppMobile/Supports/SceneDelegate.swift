63e6f3e77819a12114f152e12a4b1ec89a670c79
red-beeard <nifon1998@yandex.ru>
2021-12-07 18:08:39 +0300

    Добавил навигейшн контроллер

--- CarAppMobile/CarAppMobile/Supports/SceneDelegate.swift
+++ CarAppMobile/CarAppMobile/Supports/SceneDelegate.swift
@@ -14,7 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
-        self.window?.rootViewController = SelectBrandAssembly.build()
+        
+        let selectBrandVC = SelectBrandAssembly.build()
+        let navController = UINavigationController(rootViewController: selectBrandVC)
+        
+        self.window?.rootViewController = navController
         self.window?.makeKeyAndVisible()
     }
     
