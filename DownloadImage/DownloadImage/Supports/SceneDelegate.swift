670c00f33608363722a985c0e3b08327dfd718f6
red-beeard <nifon1998@yandex.ru>
2021-12-12 18:48:46 +0300

    Initial commit

---
+++ DownloadImage/DownloadImage/Supports/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  DownloadImage
+//
+//  Created by Red Beard on 12.12.2021.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ DownloadImage/DownloadImage/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DownloadImage
+//
+//  Created by Red Beard on 12.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .brown
+    }
+
+}
+
