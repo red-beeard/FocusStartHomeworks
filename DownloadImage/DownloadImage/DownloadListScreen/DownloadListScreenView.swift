97cdc2817b2742ccec4cc272c7ebaeb759ca788c
red-beeard <nifon1998@yandex.ru>
2021-12-13 13:40:22 +0300

    Добавил textField для поиска и прикрутил передачу данных в презентер

--- DownloadImage/DownloadImage/DownloadListScreen/DownloadListScreenView.swift
+++ DownloadImage/DownloadImage/DownloadListScreen/DownloadListScreenView.swift
@@ -13,10 +13,16 @@ protocol IDownloadListScreenView: UIView {
 
 final class DownloadListScreenView: UIView {
     
+    private enum Metrics {
+        static let searchTFHeight = CGFloat(35)
+        static let verticalSpacing = CGFloat(10)
+    }
+    
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let searchTextField = UISearchTextField()
     
     var searchHandler: ((String) -> Void)?
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -25,27 +31,55 @@ final class DownloadListScreenView: UIView {
         super.init(frame: frame)
         self.configuireView()
         self.configuireLayout()
+        self.addTapGestureToHideKeyboard()
+    }
+    
+    private func addTapGestureToHideKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(endEditing))
+        addGestureRecognizer(tapGesture)
     }
     
     private func configuireView() {
         self.backgroundColor = .systemBackground
+        self.configuireSearchTextField()
         self.configuireCollectionView()
     }
     
     private func configuireCollectionView() {
         self.collectionView.translatesAutoresizingMaskIntoConstraints = false
         self.collectionView.register(DownloadCell.self, forCellWithReuseIdentifier: DownloadCell.identifier)
+        
+        self.collectionView.backgroundColor = .brown
+    }
+    
+    private func configuireSearchTextField() {
+        self.searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.searchTextField.placeholder = "Введите URL"
+        self.searchTextField.returnKeyType = .done
+        self.searchTextField.delegate = self
     }
     
     private func configuireLayout() {
+        self.configuireSearchTextFieldLayout()
         self.configuireCollectionViewLayout()
+    }
+    
+    private func configuireSearchTextFieldLayout() {
+        self.addSubview(self.searchTextField)
+        
+        NSLayoutConstraint.activate([
+            self.searchTextField.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.searchTextField.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            self.searchTextField.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
+            self.searchTextField.heightAnchor.constraint(equalToConstant: Metrics.searchTFHeight),
+        ])
     }
     
     private func configuireCollectionViewLayout() {
         self.addSubview(self.collectionView)
         
         NSLayoutConstraint.activate([
-            self.collectionView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.collectionView.topAnchor.constraint(equalTo: self.searchTextField.bottomAnchor, constant: Metrics.verticalSpacing),
             self.collectionView.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
             self.collectionView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor),
             self.collectionView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
@@ -55,5 +89,21 @@ final class DownloadListScreenView: UIView {
 }
 
 extension DownloadListScreenView: IDownloadListScreenView {
+    
+}
+
+extension DownloadListScreenView: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let _ = textField.text {
+            self.endEditing(true)
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        self.searchHandler?(text)
+    }
     
 }
