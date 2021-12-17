3e489bd53832008d791aab775d44b81d56210104
red-beeard <nifon1998@yandex.ru>
2021-12-17 13:45:25 +0300

    Initial commit

---
+++ CompaniesEmployees/CompaniesEmployees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompaniesEmployees
+//
+//  Created by Red Beard on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
